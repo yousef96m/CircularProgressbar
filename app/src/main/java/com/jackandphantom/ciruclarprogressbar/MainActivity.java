37b4fe74037ab6c0cd27b3668ce2d92db9c48e6e
Yousef Abu Fadda <yousefa@motory.com>
2024-11-03 14:50:31 +0300

    fix and upgrade

--- app/src/main/java/com/jackandphantom/ciruclarprogressbar/MainActivity.java
+++ app/src/main/java/com/jackandphantom/ciruclarprogressbar/MainActivity.java
@@ -1,9 +1,8 @@
 package com.jackandphantom.ciruclarprogressbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
